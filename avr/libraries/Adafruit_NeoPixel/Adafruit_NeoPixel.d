c62bd46b5d1cfcfe7a108b386c0e1eba1048d791
Sanukrishnan SB <openmaker1@gmail.com>
2019-05-30 14:41:14 +0300

    attempts to setup travis ci

---
+++ avr/libraries/Adafruit_NeoPixel/Adafruit_NeoPixel.d
@@ -0,0 +1,9 @@
+../hardware/libraries/Adafruit_NeoPixel/Adafruit_NeoPixel.o: \
+ ../hardware/libraries/Adafruit_NeoPixel/Adafruit_NeoPixel.cpp \
+ ../hardware/libraries/Adafruit_NeoPixel/Adafruit_NeoPixel.h \
+ ../hardware/cores/ebot_8/Arduino.h ../hardware/cores/ebot_8/binary.h \
+ ../hardware/cores/ebot_8/WCharacter.h ../hardware/cores/ebot_8/WString.h \
+ ../hardware/cores/ebot_8/HardwareSerial.h \
+ ../hardware/cores/ebot_8/Stream.h ../hardware/cores/ebot_8/Print.h \
+ ../hardware/cores/ebot_8/Printable.h ../hardware/cores/ebot_8/new.h \
+ ../hardware/variants/ebot_8/pins_arduino.h
